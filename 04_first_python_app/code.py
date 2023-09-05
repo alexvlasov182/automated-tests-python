e0825648929edb8d481d9cb6d770db61daf7b86b
Oleksandr Vlasov <drumlife182@gmail.com>
2023-09-05 18:10:48 +0200

    Added new chapters

---
+++ 04_first_python_app/code.py
@@ -0,0 +1,4 @@
+user_age = int(input("Enter your age: "))
+month = user_age * 12
+
+print(f"Your age, {user_age}, equal to {month} months.")
---
+++ 05_lists_tuples_sets/code.py
@@ -0,0 +1,15 @@
+l = ["Bob", "Rolf", "Anne"]
+t = ("Bob", "Rolf", "Anne")
+s = {"Bob", "Rolf", "Anne"}
+
+l.append("Smith")
+l.append("John")
+l.append("Jon")
+
+s.add("Hello")
+
+l.remove("Jon")
+
+print(t)
+print(l)
+print(s)
